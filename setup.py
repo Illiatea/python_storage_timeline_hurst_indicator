fe8d0f770d0e28ba64d6c7917348f1d9bfa6971a
pysyun <48061047+pysyun@users.noreply.github.com>
2023-09-07 19:41:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='python_storage_timeline_hurst_indicator',
-    version='1.2',
+    name='storage_timeline_hurst_indicator',
+    version='1.3',
     author='Illiatea',
     author_email='illiatea2@gmail.com',
     install_requires=['numpy'],
-    py_modules=['indicators', 'processor']
+    py_modules=['storage_timeline_hurst_indicator.indicators', 'storage_timeline_hurst_indicator.processor']
 )
